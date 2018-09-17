4c7dce18f3027f4521701c2e92d95a9bdc571aff
Steve Hobbs <elkdanger@gmail.com>
2018-09-17 15:40:31 +0100

    Refactored out the cloned request

--- src/sw.js
+++ src/sw.js
@@ -15,39 +15,45 @@ self.addEventListener('install', event => {
   );
 });
 
+// Handle the 'fetch' event, raised when a resource is
+// being fetched from the network
 self.addEventListener('fetch', event => {
   event.respondWith(
     new Promise((resolve, reject) => {
+
+      // If the request should be excluded, bypass the cache entirely
       if (isExcluded(event.request.url)) {
         resolve(fetch(event.request));
       } else {
         resolve(
           caches.match(event.request).then(response => {
+
+            // If we get a cache hit, return the cached response
             if (response) return response;
 
-            var fetchRequest = event.request.clone();
+            // Execute the request
+            return fetch(event.request).then(response => {
 
-            return fetch(fetchRequest).then(response => {
+              // Clone the response, as we want to use it multiple times
               var responseToCache = response.clone();
 
-              if (isBootstrapRequest(fetchRequest)) {
+              // If it's a request to Bootstrap CDN, cache the response
+              if (isBootstrapRequest(event.request)) {
                 caches.open(cacheName).then(cache => {
-                  cache.put(fetchRequest, responseToCache);
+                  cache.put(event.request, responseToCache);
                 });
               } else {
-                if (
-                  !response ||
-                  response.status !== 200 ||
-                  response.type !== 'basic'
-                ) {
+
+                // If we don't get a positive response, exit
+                if (!response || response.status !== 200 || response.type !== 'basic') {
                   return response;
                 }
 
                 caches.open(cacheName).then(cache => {
-                  cache.put(fetchRequest, responseToCache);
+                  cache.put(event.request, responseToCache);
                 });
               }
-
+              
               return response;
             });
           })
