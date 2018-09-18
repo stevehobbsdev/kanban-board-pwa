28c9f7dcc3f31f7e4ac7924c97b0ea4056a63c2a
Steve Hobbs <elkdanger@gmail.com>
2018-09-18 10:46:36 +0100

    Rewrote service worker

--- src/sw.js
+++ src/sw.js
@@ -1,6 +1,5 @@
 const cacheName = 'kanban-cache';
 const isExcluded = f => /hot-update|sockjs/.test(f);
-const isBootstrapRequest = request => /bootstrapcdn/.test(request.url);
 
 const filesToCache = [
   ...serviceWorkerOption.assets.filter(file => !isExcluded(file)),
@@ -8,57 +7,41 @@ const filesToCache = [
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(filesToCache);
-    })
-  );
+  event.waitUntil(preCache());
 });
 
-// Handle the 'fetch' event, raised when a resource is
-// being fetched from the network
+// Caches known files up-front
+const preCache = () =>
+  caches.open(cacheName).then(cache => {
+    cache.addAll(filesToCache);
+  });
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    new Promise((resolve, reject) => {
-
-      // If the request should be excluded, bypass the cache entirely
-      if (isExcluded(event.request.url)) {
-        resolve(fetch(event.request));
-      } else {
-        resolve(
-          caches.match(event.request).then(response => {
-
-            // If we get a cache hit, return the cached response
-            if (response) return response;
-
-            // Execute the request
-            return fetch(event.request).then(response => {
-
-              // Clone the response, as we want to use it multiple times
-              var responseToCache = response.clone();
-
-              // If it's a request to Bootstrap CDN, cache the response
-              if (isBootstrapRequest(event.request)) {
-                caches.open(cacheName).then(cache => {
-                  cache.put(event.request, responseToCache);
-                });
-              } else {
-
-                // If we don't get a positive response, exit
-                if (!response || response.status !== 200 || response.type !== 'basic') {
-                  return response;
-                }
-
-                caches.open(cacheName).then(cache => {
-                  cache.put(event.request, responseToCache);
-                });
-              }
-              
-              return response;
-            });
-          })
-        );
-      }
-    })
+    fetchFromNetwork(event.request).catch(() => fetchFromCache(event.request))
   );
 });
+
+const fetchFromNetwork = request => {
+  return new Promise((resolve, reject) => {
+    fetch(request).then(response => {
+      if (!isExcluded(request.url) && response) {
+        updateCache(request, response.clone()).then(() => resolve(response));
+      } else {
+        resolve(response)
+      }
+    }, reject);
+  });
+};
+
+const fetchFromCache = request => {
+  return caches.match(request).then(response => {
+    return response || Promise.reject('failed');
+  });
+};
+
+const updateCache = (request, response) => {
+  return caches.open(cacheName).then(cache => {
+    return cache.put(request, response);
+  });
+};
